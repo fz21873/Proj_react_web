c2294f540eb9d32a796a92d40452efb1fca7d259
fz21873 <fz21873@gmail.com>
2020-06-27 02:15:08 -0400

    subiendo filmania

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React ,{ Component } from "react";
+import './header.css';
+import {Link} from 'react-router-dom';
+
+ 
+
+
+class  Header extends Component {
+
+  render(){
+      return (
+        <div  className="header">
+            
+             <Link to="/">Filmania</Link>
+             
+        </div>
+      );
+  }
+}
+
+export default Header;
